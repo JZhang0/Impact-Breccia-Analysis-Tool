e6e1d3972fafd3506985a9c241f6bc065aa39fe1
Nicolas Jacobs <njacobs6@uwo.ca>
2024-01-23 10:53:58 -0500

    Start

---
+++ src/main/java/Demo.java
@@ -0,0 +1,16 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+
+public class Demo {
+    public static void main(String[] args) {
+        // Load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat img = new Contrast().adjustContrast(ImageManipulation.getImage("C:\\Users\\Nicolas\\Downloads\\Car.jpg"));
+
+        HighGui.imshow("New Image", img);
+        HighGui.waitKey();
+        System.exit(0);
+    }
+}
