2a88d6b295b9e49797d35a27a7b5044c0f88eb33
njacobs6 <148374734+njacobs6@users.noreply.github.com>
2024-02-11 13:44:16 -0500

    Adding GUI and RGB

--- src/main/java/src/main/java/GUI/GUI.java
+++ src/main/java/src/main/java/GUI/GUI.java
@@ -1,22 +1,109 @@
 package src.main.java.GUI;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import src.main.java.Settings;
 
-public class GUI extends JFrame
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.Image;
+
+public class GUI extends javax.swing.JFrame
 {
-    public GUI()
+	private static JFrame frame;
+	private static JPanel buttonPanel;
+	private static JPanel mainPanel;
+
+	//Create the GUI for the application
+	public GUI()
+	{
+		frame = new JFrame("Impact Breccia Analysis Tool v" + Settings.VERSION);
+		frame.setLayout(new BorderLayout());
+		frame.setSize(Settings.DEFAULT_WIDTH, Settings.DEFAULT_HEIGHT);
+
+		//Create button panel and main panel
+		buttonPanel = createButtonPanel();
+		mainPanel = createMainPanel();
+
+		//Add button panel and main panel to the frame
+		frame.add(buttonPanel, BorderLayout.NORTH);
+		frame.add(mainPanel, BorderLayout.CENTER);
+
+		//Set application size
+		if (Settings.LAUNCH_MAXIMIZED)
+			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	//Create the button panel at the top of the screen
+    private static JPanel createButtonPanel()
     {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(Settings.getDefaultWidth(), Settings.getDefaultHeight());
-        this.setTitle("Impact Breccia Analysis Tool v" + Settings.getVersion());
-        this.setLocationRelativeTo(null);
+        JPanel topPanel = new JPanel();
+		JButton[] buttons = new JButton[12];
 
-        //ImagePannel imagePannel = new ImagePannel();
+		buttons[0] = new ButtonTMP();
+		buttons[1] = new ButtonTMP();
+		buttons[2] = new RGBGUI(frame);
+		buttons[3] = new ButtonTMP();
+		buttons[4] = new ButtonTMP();
+		buttons[5] = new ButtonTMP();
+		buttons[6] = new ButtonTMP();
+		buttons[7] = new ButtonTMP();
+		buttons[8] = new ButtonTMP();
+		buttons[9] = new ButtonTMP();
+		buttons[10] = new ButtonTMP();
+		buttons[11] = new ButtonTMP();
 
-        //this.add(imagePannel);
+		//Initialize the bar's layout
+        topPanel.setLayout(new GridLayout(1, buttons.length));
 
-        this.setVisible(true);
+		for (JButton button : buttons)
+		{
+			topPanel.add(button);
+			adjustButtonSize(button, frame, buttons.length);
+		}
+
+        return topPanel;
     }
+
+	//Create the main panel that displays the image
+	private static JPanel createMainPanel()
+	{
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		JLabel helloLabel = new JLabel("Drop image here");
+
+		helloLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		mainPanel.add(helloLabel, BorderLayout.CENTER);
+
+		return mainPanel;
+	}
+
+	//Update button size
+    private static void adjustButtonSize(JButton button, JFrame frame, int button_total) {
+        Icon icon = button.getIcon();
+        ImageIcon imageIcon = (ImageIcon) icon;
+        Image image = imageIcon.getImage();
+        int size = Math.min(frame.getHeight(), frame.getWidth()) / button_total; // Divide by the number of buttons
+        Image scaledImage = image.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+
+		button.setIcon(new ImageIcon(scaledImage));
+        button.setPreferredSize(new Dimension(size, size));
+    }
+
+	//This sets the main area to the image sample given
+	public static void render(byte[] image)
+	{
+		// Replace main panel with an image
+		ImageIcon imageIcon = new ImageIcon(image);
+		JLabel imageLabel = new JLabel(imageIcon);
+
+		mainPanel.removeAll();
+		mainPanel.add(imageLabel, BorderLayout.CENTER);
+		mainPanel.revalidate();
+		mainPanel.repaint();
+	}
 }
---
+++ src/main/java/utils/Processing/RGB.java
@@ -0,0 +1,43 @@
+package utils.Processing;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import src.main.java.GUI.MainImage;
+
+import java.util.ArrayList;
+
+public class RGB
+{
+	private static int channel = -1; //The channel that the image has been split to
+	private static boolean updated = false; //Has the image already had its channel's split?
+
+	//Split the channels to query_channel
+	public static Mat split(int query_channel)
+	{
+		if (query_channel > 2 || query_channel < 0 || updated)
+			return MainImage.getImageMat();
+
+		ArrayList<Mat> channels = new ArrayList<>(3);
+		Core.split(MainImage.getImageMat(), channels);
+
+		channel = query_channel;
+
+		return channels.get(query_channel);
+	}
+
+	//Save the current query_channel as the image for further processing
+	public static void save()
+	{
+		if (channel > -1)
+		{
+			MainImage.setImage(split(channel));
+			updated = true;
+		}
+	}
+
+	//Reset the internally saved channel
+	public static void resetChannel()
+	{
+		channel = -1;
+	}
+}
